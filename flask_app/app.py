c182dbbb4e411b5678319ef770a4bf0a5b32f393
MichielNijmeijer <michielenn@gmail.com>
2025-03-25 17:35:35 +0100

    Expanded import resolving, tracking and structuring

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,19 +1,34 @@
+import argparse
+import webbrowser
+
 from flask import Flask, render_template
 
 from trace_imports import build_import_graph
 
-app = Flask(__name__)
 
-file_to_inspect = 'file_to_trace.py'
-traced_imports = build_import_graph(file_to_inspect)
-graph_data = traced_imports.get_parent_child_dict()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filepath", type=str)
+    parser.add_argument("--track_conditional_imports", action='store_true')
+    args = parser.parse_args()
+    file_to_inspect = args.filepath
+    display_conditional = args.track_conditional_imports
 
+    traced_imports = build_import_graph(file_to_inspect, display_conditional)
+    graph_data = traced_imports.get_parent_child_dict()
+    app = Flask(__name__)
 
-# Root route to serve the HTML page
-@app.route('/')
-def home():
-    return render_template('force_graph.html', graph_data=graph_data)
+    # Root route to serve the HTML page
+    @app.route('/')
+    def home():
+        return render_template('force_graph.html', graph_data=graph_data)
+
+    host_address = '127.0.0.1'
+    port = 5000
+    full_address = f"http://{host_address}:{str(port)}"
+    webbrowser.open(full_address)
+    app.run(host=host_address, port=port)
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    main()
--- trace_imports.py
+++ trace_imports.py
@@ -1,5 +1,6 @@
 import ast
 import hashlib
+import os
 from dataclasses import dataclass
 from enum import StrEnum
 from pathlib import Path
@@ -9,6 +10,12 @@ class NodeType(StrEnum):
     ROOT = 'root'
     LOCAL = 'local'
     THIRD_PARTY = 'third_party'
+
+
+@dataclass
+class ImportNode:
+    name: str
+    conditional: bool
 
 
 @dataclass
@@ -24,6 +31,7 @@ class Node:
 
 class ImportTracker:
     def __init__(self):
+        self._root_node: str | None = None
         self._parent_nodes: set[Node] = set()
         self._child_nodes: set[Node] = set()
         self._connections: set[tuple[Node, Node]] = set()
@@ -44,28 +52,56 @@ class ImportTracker:
         graph_connections = [{"source": parent_node.file_path, "target": child_node.file_path} for parent_node, child_node in self._connections]
         return {"nodes": graph_nodes, "links": graph_connections}
 
+    def set_root_node(self, starting_node: Node):
+        self._root_node = starting_node.file_path
 
-def find_imports_in_file(file_path: str) -> list[str]:
-    imports = []
+    def is_root(self, node_path: str) -> bool:
+        return self._root_node == node_path
 
+
+def categorize_nodes(node) -> list[ImportNode]:
+
+    parent_node = getattr(node, 'parent', None)
+    if parent_node is None:
+        raise ImportError("Import has no parent.")
+
+    import_nodes = []
+    # anything not directly in the file is a conditonal import in our book
+    is_conditional = not isinstance(node.parent, ast.Module)
+    if isinstance(node, ast.Import):
+        for alias in node.names:
+            import_nodes.append(ImportNode(name=alias.name, conditional=is_conditional))
+    elif isinstance(node, ast.ImportFrom):
+        for alias in node.names:
+            import_nodes.append(ImportNode(name=f"{node.module}.{alias.name}", conditional=is_conditional))
+    else:
+        raise ImportError(f"Could not categorize node as it is not an import, but a '{type(node)}':\n{node}")
+    return import_nodes
+
+
+def find_imports_in_file(file_path: str) -> list[ImportNode]:
+    imports: list[ImportNode] = []
+
+    # get the tree
     with open(file_path, "r") as f:
         tree = ast.parse(f.read(), filename=file_path)
 
+    # loop through tree and set parent <-> child relationships for all nodes
     for node in ast.walk(tree):
-        if isinstance(node, ast.Import):
-            for alias in node.names:
-                imports.append(alias.name)
-        elif isinstance(node, ast.ImportFrom):
-            for alias in node.names:
-                imports.append(f"{node.module}.{alias.name}")
+        for child in ast.iter_child_nodes(node):
+            child.parent = node
+
+    for node in ast.walk(tree):
+        if isinstance(node, (ast.Import, ast.ImportFrom)):
+            imported_nodes = categorize_nodes(node)
+            imports.extend(imported_nodes)
     return imports
 
 
-def build_import_graph(input_path: str) -> ImportTracker:
+def build_import_graph(input_path: str, track_conditionals: bool) -> ImportTracker:
     """
     Builds a directed graph (parent -> child) of imports.
     """
-    graph_connections = ImportTracker()
 
     def get_imports_from_file(current_node: Node) -> None:
         """
@@ -74,11 +110,13 @@ def build_import_graph(input_path: str) -> ImportTracker:
 
         imports_in_file = find_imports_in_file(current_node.file_path)
 
-        for import_address in imports_in_file:
-            split_import = import_address.split('.')
+        for import_node in imports_in_file:
+            if not track_conditionals and import_node.conditional:
+                continue
+            split_import = import_node.name.split('.')
 
             root_package = split_import[0]
-            if not Path(root_package).is_dir():
+            if not (working_dir / root_package).is_dir() and not (working_dir / (root_package + '.py')).is_file():
                 # this means it's a 3rd party import, stop after logging this import
                 child_node = Node(name=root_package, file_path=root_package, type=NodeType.THIRD_PARTY)
                 graph_connections.add_connection(current_node, child_node)
@@ -89,7 +127,10 @@ def build_import_graph(input_path: str) -> ImportTracker:
                 # if it's the last entry in the split import, check the .py file
                 if i == max_range - 1:
                     possible_file = '/'.join(split_import[:i + 1]) + '.py'
-                    child_node = Node(name=possible_file, file_path=possible_file, type=NodeType.LOCAL)
+                    if not (working_dir / possible_file).is_file():
+                        continue
+                    node_type = NodeType.ROOT if graph_connections.is_root(possible_file) else NodeType.LOCAL
+                    child_node = Node(name=possible_file, file_path=possible_file, type=node_type)
                     graph_connections.add_connection(current_node, child_node)
                     # check if we have already checked this file
                     if possible_file not in graph_connections.checked_files:
@@ -97,28 +138,22 @@ def build_import_graph(input_path: str) -> ImportTracker:
                     continue
                 # else check for existence of __init__, and trace it
                 base_path = '/'.join(split_import[:i + 1])
-                if current_node.file_path.startswith(base_path):
-                    # Prevent example_production/libs/pickle.py that imports from example_production/packages/test.py creating a link
-                    # to the example_production init again. Could be left in, but its overly verbose IMO
-                    continue
                 possible_file = base_path + '/__init__.py'
-                if Path(possible_file).is_file():
-                    child_node = Node(name=possible_file, file_path=possible_file, type=NodeType.LOCAL)
+                if (working_dir / possible_file).is_file():
+                    node_type = NodeType.ROOT if graph_connections.is_root(possible_file) else NodeType.LOCAL
+                    child_node = Node(name=possible_file, file_path=possible_file, type=node_type)
                     graph_connections.add_connection(current_node, child_node)
                     if possible_file not in graph_connections.checked_files:
                         get_imports_from_file(child_node)
+
+    graph_connections = ImportTracker()
+    working_dir = Path.cwd()
 
     starting_node = Node(
         name=input_path,
         file_path=input_path,
         type=NodeType.ROOT
     )
+    graph_connections.set_root_node(starting_node)
     get_imports_from_file(starting_node)
     return graph_connections
-
-
-# Run the script with the desired directory
-if __name__ == "__main__":
-    directory = "file_to_trace.py"
-    graph = build_import_graph(directory)
-    pass
